1196a8300e927c170377066c6771125362258ac0
Nguyen <len054@usask.ca>
2023-10-06 15:25:06 -0600

    a sketch of the recursion, not working. Need to add test

--- src/main/Change.java
+++ src/main/Change.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 public class Change {
     static int desiredAmount;
     static int[] coinValues;
+    static int maxLevel;
 
     public static void main(String[] args) {
         try {
@@ -14,7 +15,13 @@ public class Change {
             System.err.println(e.getMessage());
             System.exit(1);
         }
-        findAllPosibility();
+        ArrayList<int[]> posibilities = findAllPosibility();
+        if (posibilities.size() == 0) {
+            System.out.println("IMPOSSIBLE!");
+        } else {
+            System.out.println("The number of posibilitties: " + posibilities.size());
+            System.out.println("The minimum number of coins: " + Arrays.toString(posibilities.get(-1)));
+        }
     }
 
     private static int[] validateCommandLineArgs(String[] args)
@@ -44,15 +51,52 @@ public class Change {
                 desiredAmount = arr[0];
                 System.out.println("Desired amount: " + desiredAmount);
                 coinValues = Arrays.copyOfRange(arr, 1, arr.length);
+                maxLevel = coinValues.length - 1;
                 System.out.println("Given coin values: " + Arrays.toString(coinValues));
                 return arr;
             }
         }
     }
 
-    public static ArrayList<Integer> findAllPosibility() {
-        System.out.println("hello world");
-        return null;
+    public static ArrayList<int[]> findAllPosibility() {
+        // Sort coin values
+        Arrays.sort(coinValues);
+
+        ArrayList<int[]> possibilities = new ArrayList<>();
+
+        int[] currentSearch = new int[coinValues.length];
+        Arrays.fill(currentSearch, 0);
+        search(currentSearch, 0, 0, possibilities);
+
+        return possibilities;
+    }
+
+    public static void search(int[] currentSearch, int currentLevel, int currentAmount,
+            ArrayList<int[]> possibilities) {
+        // Base cases
+        if (currentAmount == desiredAmount) {
+            possibilities.add(currentSearch);
+        } else if (currentAmount > desiredAmount) {
+            // do nothing;
+        }
+        // Recursion
+        // Max repetition of the current coin value
+        else {
+            int amtToFill = desiredAmount - currentAmount;
+            int currentCoinValue = coinValues[currentAmount];
+            if (currentCoinValue > desiredAmount) {
+                // do nothing
+                return;
+            }
+            int numNodeToSearch = Math.floorDiv(amtToFill, currentCoinValue);
+            // i is the number of repetition of the current coin value
+            for (int i = numNodeToSearch; i >= 0; i--) {
+                int[] newSearch = currentSearch.clone();
+                System.out.println("Size of new search: " + newSearch.length);
+                newSearch[currentLevel] = i;
+                search(newSearch, currentLevel + 1, currentAmount + i * currentCoinValue, possibilities);
+            }
+        }
     }
 
 }
