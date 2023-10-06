cf36c7973f86486c52769ad5ddd83d0999aaa00a
Nguyen <len054@usask.ca>
2023-10-06 14:27:44 -0600

    refactor the validation of commandline arguments

--- src/main/Change.java
+++ src/main/Change.java
@@ -1,25 +1,42 @@
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Optional;
 
 public class Change {
 
     public static void main(String[] args) {
-        if (args.length == 0) {
-            System.err.println("There is no arguments!");
+        try {
+            int[] validated_args = validateCommandLineArgs(args);
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
             System.exit(1);
+        }
+
+        findAllPosibility();
+    }
+
+    private static int[] validateCommandLineArgs(String[] args)
+            throws IllegalArgumentException {
+        if (args.length == 0) {
+            throw new IllegalArgumentException("There is no arguments!");
         } else {
             int size = args.length;
             int[] arr = new int[size];
-            try {
-                for (int i = 0; i < size; i++) {
-                    arr[i] = Integer.parseInt(args[i]);
+            for (int i = 0; i < size; i++) {
+                Integer current = null;
+                try {
+                    current = Integer.parseInt(args[i]);
+                } catch (Exception e) {
+                    throw new IllegalArgumentException(
+                            "There is an argument other than integer type: \n" + e.getMessage());
                 }
-            } catch (Exception e) {
-                System.err.println("There is an argument other than integer type: \n" + e.getMessage());
-                System.exit(1);
+
+                if (current < 0) {
+                    throw new IllegalArgumentException("Amount or coin values can't be smaller than 0!");
+                }
             }
+            return arr;
         }
-        findAllPosibility();
     }
 
     public static ArrayList<Integer> findAllPosibility() {
