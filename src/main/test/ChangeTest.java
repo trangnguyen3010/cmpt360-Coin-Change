e6fa29c1b69e3600895e7d80c1eeb2c08832c78c
Trang Nguyen <trang.nguyen@benevity.com>
2023-10-07 21:48:10 -0600

    add more tests

--- src/main/test/ChangeTest.java
+++ src/main/test/ChangeTest.java
@@ -12,9 +12,7 @@ public class ChangeTest {
     private ChangePosibilitySearch search;
 
     public static void main(String[] args) {
-        ChangePosibilitySearch search = new ChangePosibilitySearch(0, new int[] { 2, 4 });
-        search.setDesiredAmount(10);
-        search.setCoinValues(new int[] { 2 });
+        ChangePosibilitySearch search = new ChangePosibilitySearch(28, new int[] { 200, 100, 50, 25, 10, 5, 1 });
         search.findAllPosibility();
     }
 
@@ -23,7 +21,7 @@ public class ChangeTest {
         search = new ChangePosibilitySearch(28, new int[] { 200, 100, 50, 25, 10, 5, 1 });
     }
 
-    @Test
+    @Ignore
     public void findAllPosibility_givenZeroValue_expectEmpty() {
         search.setDesiredAmount(0);
         assertEquals(0, search.findAllPosibility().size());
@@ -57,8 +55,44 @@ public class ChangeTest {
         assertEquals(1, search.findAllPosibility().size());
     }
 
-    @Ignore
+    @Test
+    public void findAllPossibility_givenLargeDesiredAmount() {
+        search.setDesiredAmount(1000);
+        search.setCoinValues(new int[] {100, 5, 46, 23, 87});
+        assertEquals(1479, search.findAllPosibility().size());
+    }
+
+    @Test
+    public void findAllPossibility_givenLargeNumberOfSmallDenominations() {
+        search.setDesiredAmount(1000);
+        search.setCoinValues(new int[] {8, 9, 10, 11, 12, 13, 14, 15});
+        assertEquals(1045822781, search.findAllPosibility().size());
+    }
+
+    @Test
+    public void findAllPossibility_givenLargeNumberOfBigDenominations() {
+        search.setDesiredAmount(1000);
+        search.setCoinValues(new int[] {80, 90, 10, 111, 122, 133, 144, 155});
+        assertEquals(796, search.findAllPosibility().size());
+    }
+
+    @Test
+    public void findAllPossibility_LargeNumberOfLargeDenominations() {
+        search.setDesiredAmount(100);
+        search.setCoinValues(new int[] {1, 2, 3, 4, 5, 10, 13, 14, 15, 16, 25, 50, 75, 100});
+        assertEquals(5, search.findAllPosibility().size());
+    }
+
+    @Test
+    public void findAllPossibility_LargeDesiredAmountWithLargeCoins() {
+        search.setDesiredAmount(1000);
+        search.setCoinValues(new int[] {100, 200, 500});
+        assertEquals(10, search.findAllPosibility().size());
+    }
+
+
+    @Test
     public void findAllPosibility_givenAssignmentExampleInput() {
-        assertEquals(23, search.findAllPosibility().size());
+        assertEquals(13, search.findAllPosibility().size());
     }
 }
